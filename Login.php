3d3e61d7c95a2cec6d462590fe0fd8e6a71921fb
JamesLin4113 <ll4113@ic.ac.uk>
2016-06-10 11:08:25 +0100

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,47 @@
+<?php
+	//myhost, my_user, my_password, my_db
+	//Returns an object which represents the connection to a MySQL Server. 
+    $con = mysqli_connect("mysql9.000webhost.com", "a1339558_Lin", "bboxx2016", "a1339558_BBoxx");
+    
+
+	$num = mysqli_prepare($con, "UPDATE user SET NumOfPending = (SELECT COUNT(*) FROM PendingSurvey) WHERE username = ? ");
+	mysqli_stmt_bind_param($num, "s", $_POST["username"]);
+	mysqli_stmt_execute($num);
+	
+	//Prepares the SQL query, and returns a statement handle to be used for further operations on the statement. The query must consist of a single SQL statement. 
+    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ? AND password = ?");//select user from database
+    
+	//Bind variables for the parameter markers in the SQL statement that was passed to mysqli_prepare(). 
+	mysqli_stmt_bind_param($statement, "ss", $_POST["username"], $_POST["password"]);
+	
+	//Executes a query that has been previously prepared using the mysqli_prepare() function. 
+	//When executed any parameter markers which exist will automatically be replaced with the appropriate data. 
+	//Returns TRUE on success or FALSE on failure. 
+    mysqli_stmt_execute($statement);
+    
+	//You must call mysqli_stmt_store_result() for every query that successfully produces a result set (SELECT, SHOW, DESCRIBE, EXPLAIN), 
+	//if and only if you want to buffer the complete result set by the client, so that the subsequent mysqli_stmt_fetch() call returns buffered data.
+    mysqli_stmt_store_result($statement);
+	
+	//Binds columns in the result set to variables. 
+    mysqli_stmt_bind_result($statement, $name, $username, $age, $password, $NumOfPending, $points);
+    
+    $response = array();
+    $response["success"] = false;  
+    
+	//returns everything from the database
+    while(mysqli_stmt_fetch($statement)){//Fetch the result from a prepared statement into the variables bound by mysqli_stmt_bind_result().
+        $response["success"] = true;  
+        $response["name"] = $name;
+        $response["age"] = $age;
+        $response["username"] = $username;
+        $response["password"] = $password;
+		$response["NumOfPending"] = $NumOfPending;//****pending
+		$response["points"] = $points;
+		//****update number of pending survey*****	
+	    }
+    
+	//echo Outputs all parameters. No additional newline is appended. 
+	//json_encode — Returns the JSON representation of a value
+    echo json_encode($response);
+?>
